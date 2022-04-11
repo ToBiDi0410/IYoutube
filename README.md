4a8fa53d8e03b3b734fa98a3c6e1bfa62aca4532
Tobias Dickes <hanstodi2@gmail.com>
2022-04-11 18:06:37 +0200

    Fix documentation and disable Debug

--- README.md
+++ README.md
@@ -15,51 +15,7 @@ IYoutube is the ultimate dirty Youtube API Client covering the most Parts of the
 
 #### Usage
 
-##### Adapters
-In order to use this Libary you have to implement an Storage Adapter and an HTTPClient Adapter. These Adapters must implement the according interfaces ([StorageAdapter](./src/interfaces/StorageAdapter.ts) & [HTTPClient](./src/interfaces/HTTPClient.ts)). You can find two sample Adapters for the common Node Packages in the [Test Folder](./test/).
-
-For most parts, this should be enough for use. So just include them like in the [Test File](./test/main.ts) and install the according Packages (node-fetch)
-
-##### Authenticator
-Currently, the entire Library requires you to be Authenticated. You can just use the Authenticator to create the needed Tokens. 
-
-```js
-var ytClient = YOUR_INSTANCE_OF_IYOUTUBE;
-await ytClient.init();
-
-//Check if there needs to be a relogin
-if(ytClient.authenticator.requiresLogin()) {
-
-    //Aquire a new Device and Login Code
-    var codes = await ytClient.authenticator.getNewLoginCode();
-    console.log("User Code: " + codes.userCode);
-
-    /*❗Enter the User Code at https://google.com/device and Login
-    This will resolve the loadTokensWithDeviceCode Promise and you are now authenticated */
-
-
-    await ytClient.authenticator.loadTokensWithDeviceCode(codes.deviceCode);
-    /* The Authenticator will save it's tokens in IYoutubeTokens.json */
-}
-
-//Do your calls to the API
-```
-
-##### Continuated List
-```js
-var list = await ytClient.search("test", SearchType.VIDEO);
-
-//Load the next Results
-list.loadFurther(); /* Returns the new Results */
-
-//All Results
-list.results
-
-//End reached
-list.endReached
-
-```
-Searches and Playlist Video Lists are using the Continuated List format. You have to load the next Results if you want to expand the list. You can also check if the list is at it's end
+###### Please look at the Basic [Example](./test/test.js)
 
 #### TODO
 You can find the current TODO list [here](./todo.md)
--- test/test.js
+++ test/test.js
@@ -1,20 +1,39 @@
+//Import the nodeInst Export of IYoutube
+//This export is already filled with an Storage Adapter and HTTPClient
+//If you just want to use this Library generally with NodeJS you can ignore what this means
 const ytClient = require("../").nodeInst;
-
-console.log("HEY!");
+// !IMPORTANT! You have to replace the ../ with IYoutube
 
 (async function() {
+    //Init the Client
     await ytClient.init();
+
+    //Check if there needs to be User Login
     if (ytClient.authenticator.requiresLogin()) {
+        //Get new Codes for Login
         var codes = await ytClient.authenticator.getNewLoginCode();
 
+        //Display message with Code and URL for User Action
         console.log("This program requires Login at: " + codes.userUrl);
         console.log("Use the following Code: " + codes.userCode);
 
+        //Load the Tokens with the Device Code (will wait until finished)
         await ytClient.authenticator.loadTokensWithDeviceCode(codes.deviceCode);
         console.log("Thank you for login!");
     }
+    //We are now Authenticated and can get started
 
-    var testPlaylist = await ytClient.getUser().getWatchLaterPlaylist();
+    var testPlaylist = await ytClient.getUser().getLikedPlaylist(); //Get the Watch Later Playlist
     await testPlaylist.loadAll(); //Load all Details we can get
-    console.log(testPlaylist.canLike);
+    console.log(testPlaylist.videoCount);
+
+    var testPlaylistVideos = testPlaylist.getContinuatedList(); //Get the Continuated List
+    await testPlaylistVideos.loadFurhter(); //Load Batch of Videos into List
+    console.log(testPlaylistVideos.results.length);
+    await testPlaylistVideos.loadFurhter(); //Load next Batch of Videos into List
+    console.log(testPlaylistVideos.results.length);
+    console.log(testPlaylistVideos.endReached);
+
+    // The Video Amount per Batch varies from List Type (e.g Playlist, Search) but also from Randomness of the Youtube API
+    // Hint: loadFurther() returns the new Videos and adds them to the results at the same Time
 })();
